b965609409f920e13c1f13940e7c2edff29851eb
flutter <akashvs64731@gmail.com>
2023-10-13 11:47:17 +0530

    done

--- lib/firebase_crud_operation/addtask.dart
+++ lib/firebase_crud_operation/addtask.dart
@@ -1,4 +1,8 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class Addtask extends StatefulWidget {
   const Addtask({super.key});
 
@@ -7,6 +11,18 @@ class Addtask extends StatefulWidget {
 }
 
 class _AddtaskState extends State<Addtask> {
+  final contentController = TextEditingController();
+  final timestampController = TextEditingController();
+  final CollectionReference task =
+      FirebaseFirestore.instance.collection('tasks');
+  Addtask() {
+    final data = {
+      'content': contentController.text,
+      'timestamp': timestampController.text,
+    };
+    task.add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +34,33 @@ class _AddtaskState extends State<Addtask> {
       body: Column(
         children: [
           Padding(
-              padding:EdgeInsets.all(8.0),
-          child: TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(label: Text("timestamp")),
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: contentController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), label: Text("content")),
+            ),
           ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: TextField(
+              controller: timestampController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), label: Text("Timestamp")),
+            ),
           ),
           ElevatedButton(
               style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.teal)
-              ),
-              onPressed: null, child: Text(
-              "submit",
-            style: TextStyle(color: Colors.white),
-          ))
+                  backgroundColor: MaterialStatePropertyAll(Colors.teal)),
+              onPressed: () {
+                Addtask();
+                Navigator.pop(context);
+              },
+              child: Text(
+                "submit",
+                style: TextStyle(color: Colors.white),
+              ))
         ],
       ),
     );
