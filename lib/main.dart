d58ddf088099a079120fe071605e3a01fd680365
akashvs <akashvs64731@gmail.com>
2023-11-10 12:13:12 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter16firebase2/bloc/counter_screen.dart';
 import 'package:flutter16firebase2/physics_animation/physics_animation_sample2.dart';
-void main() async{
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/counter/counter_bloc.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: PhysicsAnimation(),
+    return BlocProvider(
+      create: (context) => CounterBloc(),
+      child: MaterialApp(
+        home: CounterScreen(),
+      ),
     );
   }
 }
